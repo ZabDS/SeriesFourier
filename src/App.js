bf5b85513cc42beaf8eec4b117a4c3fbea3568e8
ZabDS <zabdiel1297@gmail.com>
2021-03-14 22:25:15 -0600

    Primer version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries, HorizontalGridLines} from 'react-vis';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let N=10000;
+
+function serieEscalonada(x){
+      let n = 1;
+      let sum = 0;
+      while(n<=N){
+        sum += (2 / (n * Math.PI)) * (1-(Math.pow((-1),n))) * Math.sin(n*x);
+        n++;
+      }
+      return sum;
+}
+
+function getData(){
+  return([...new Array(100)].map((row,index) => ({
+    x: index+1,
+    y: serieEscalonada(index+1)
+  })));
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>N = {N}</h1>
+        <XYPlot height={400} width={1000}>
+        <HorizontalGridLines />
+          <LineSeries data={getData()} />
+        </XYPlot>
+      </div>
+    );
+  }
 }
 
 export default App;
